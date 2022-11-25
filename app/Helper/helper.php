a44a71f0fe4a5461288174fd8f1762b809832c0e
AhmedAbuHussein <ahmed.shaker.fci.fcu@gmail.com>
2022-11-25 02:27:25 +0200

    althiga init

---
+++ app/Helper/helper.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+
+if (!function_exists("uploadImage")) {
+
+    function uploadImage(UploadedFile $file, $oldFileName = null, $prefix="", $resize = false, $width = 200, $height = 200, $folder = 'images')
+    {
+        if (!Storage::exists("$folder")) {
+            Storage::makeDirectory("$folder");
+        }
+        if ($oldFileName && $oldFileName != "default.png") {
+            try {
+                Storage::delete(["$folder/$oldFileName", "$oldFileName"]);
+            } catch (\Throwable $th) {
+            }
+        }
+        $extension = $file->getClientOriginalExtension();
+        $name = $prefix . '.' . Str::random() . '.' . ($extension ? $extension : 'png') ;
+        if ($resize) {
+            $directory = "$folder/$name";
+            if (!Storage::exists("$folder")) {
+                Storage::makeDirectory("$folder");
+            }
+            $image = Image::make($file)->resize($width, $height);
+            $image->save(storage_path("app/public/$directory"));
+        } else {
+            if(! (Storage::putFileAs("$folder", $file, $name))){
+                throw new Exception("Cant save this file", 400);
+            }
+        }
+        return "$folder/$name";
+    }
+}
+
+
+?>
